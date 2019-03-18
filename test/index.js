9b91e092e3fade2d76b1011caf1e3199d021d3cb
Connor Turland <connorturland@gmail.com>
2019-03-18 12:24:22 -0400

    update for 0.0.6

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 const { Config, Scenario } = require('@holochain/holochain-nodejs')
 Scenario.setTape(require('tape'))
-const dnaPath = 'dist/bundle.json'
+const dnaPath = 'dist/holochain-rust-todo.dna.json'
 const dna = Config.dna(dnaPath, 'happs')
 const agentAlice = Config.agent('alice')
 const instanceAlice = Config.instance(agentAlice, dna)
