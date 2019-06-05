89dde3daa26d47da85ed10d829754b87cc319fde
James Ray <16969914+jamesray1@users.noreply.github.com>
2019-06-05 13:21:18 +1000

    Fix and update index.js for 0.0.17-alpha2.

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
+// This test file uses the tape testing framework.
+// To learn more, go here: https://github.com/substack/tape
 const { Config, Scenario } = require('@holochain/holochain-nodejs')
 Scenario.setTape(require('tape'))
-const dnaPath = 'dist/holochain-rust-todo.dna.json'
+const dnaPath = "./dist/holochain-rust-todo.dna.json"
 const dna = Config.dna(dnaPath, 'happs')
 const agentAlice = Config.agent('alice')
 const instanceAlice = Config.instance(agentAlice, dna)
@@ -31,7 +33,7 @@ scenario.runTape('Can get a list with items', async (t, { alice }) => {
   const listAddr = createResult.Ok
 
   await alice.callSync('lists', 'add_item', { list_item: { text: 'Learn Rust', completed: true }, list_addr: listAddr })
-  await alice.callSync('lists', 'add_item', { list_item: { text: 'Master Holochain', completed: false }, list_addrgit: listAddr })
+  await alice.callSync('lists', 'add_item', { list_item: { text: 'Master Holochain', completed: false }, list_addr: listAddr })
 
   const getResult = await alice.callSync('lists', 'get_list', { list_addr: listAddr })
   console.log(getResult)
